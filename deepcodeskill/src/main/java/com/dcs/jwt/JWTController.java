2c6fcdc012682be8d24f6fb5e2100f487a2e18ab
adriii28 <adrigallego28@gmail.com>
2023-12-12 19:19:13 +0100

    cors

--- deepcodeskill/src/main/java/com/dcs/jwt/JWTController.java
+++ deepcodeskill/src/main/java/com/dcs/jwt/JWTController.java
@@ -1,12 +1,14 @@
 package com.dcs.jwt;
 
 import org.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,7 @@ import com.dcs.dto.Role;
 import com.dcs.dto.User;
 import com.dcs.exception.UserNotFoundException;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/auth")
 public class JWTController {
