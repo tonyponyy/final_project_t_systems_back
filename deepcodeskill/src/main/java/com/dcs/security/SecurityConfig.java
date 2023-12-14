8927b8bd8af986d654850ceac51f88f8e7840bad
DESKTOP-SUK4OIH\anton <antoniandreut@gmail.com>
2023-12-14 15:28:07 +0100

    test cors

--- deepcodeskill/src/main/java/com/dcs/security/SecurityConfig.java
+++ deepcodeskill/src/main/java/com/dcs/security/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		boolean security = true;
-		if (security) {
+		if (security) { 
 			return  http.cors().and().csrf().disable().authorizeHttpRequests().requestMatchers(UN_SECURED_URLs).permitAll().and()
 					.authorizeHttpRequests().requestMatchers(SECURED_ADMIN).hasAuthority("admin")
 					.requestMatchers(SECURED_USER).hasAuthority("user").requestMatchers(SECURED_HR_AND_USER).hasAnyAuthority("hr","user")
