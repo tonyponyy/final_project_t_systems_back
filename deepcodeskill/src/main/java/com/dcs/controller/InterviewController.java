fddebe312605d6a29399ba1da39d5a319a050111
DESKTOP-SUK4OIH\anton <antoniandreut@gmail.com>
2023-12-14 15:25:56 +0100

    test cors

--- deepcodeskill/src/main/java/com/dcs/controller/InterviewController.java
+++ deepcodeskill/src/main/java/com/dcs/controller/InterviewController.java
@@ -35,73 +35,77 @@ import com.dcs.service.IUserTestServiceImpl;
 @RestController
 @RequestMapping("/interviews")
 public class InterviewController {
-	
+
 	@Autowired
 	IInterviewServiceImpl iSer;
-	
+
 	@Autowired
 	IUserServiceImpl iuuSer;
-	
+
 	@Autowired
 	IUserInterviewServiceImpl iuSer;
-	
+
 	@Autowired
 	IUserTestServiceImpl iutSer;
-	
-	
-	private InterviewBasic ConvertInterview(Interview interview){
-		return new InterviewBasic(interview.getId(),interview.getTitle(),interview.getEnd_date(),interview.getSkills());
+
+	private InterviewBasic ConvertInterview(Interview interview) {
+		return new InterviewBasic(interview.getId(), interview.getTitle(), interview.getEnd_date(),
+				interview.getSkills());
 	}
-	
-	/*ROLE USUARIO y RH 
-	  Lista todas las entrevistas paginadas*/
+
+	/*
+	 * ROLE USUARIO y RH Lista todas las entrevistas paginadas
+	 */
 	@GetMapping("/paginated_interviews")
-	public ResponseEntity<List<InterviewBasic>>getPaginatedInterviewBasic(
-			@RequestParam(defaultValue = "0")int page,
-			@RequestParam(defaultValue = "5") int size){
-		
-		Page<Interview> interviewPage = iSer.getPaginatedInterviewBasic(PageRequest.of(page,size));
-		List<InterviewBasic> interviews_basic = interviewPage.stream().map(this::ConvertInterview).collect(Collectors.toList());
-		
-		return new ResponseEntity<>(interviews_basic,HttpStatus.OK);
+	public ResponseEntity<List<InterviewBasic>> getPaginatedInterviewBasic(@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "5") int size) {
+
+		Page<Interview> interviewPage = iSer.getPaginatedInterviewBasic(PageRequest.of(page, size));
+		List<InterviewBasic> interviews_basic = interviewPage.stream().map(this::ConvertInterview)
+				.collect(Collectors.toList());
+
+		return new ResponseEntity<>(interviews_basic, HttpStatus.OK);
 	}
-	
-	/*ROLE USUARIO y RH
-	  Buscar entrevista por titulo*/
+
+	/*
+	 * ROLE USUARIO y RH Buscar entrevista por titulo
+	 */
 	@GetMapping("/search_by/{title}")
-	public ResponseEntity<List<InterviewBasic>>getPaginatedInterviewBasicTitle(
-			@PathVariable(name="title") String title,
-			@RequestParam(defaultValue = "0")int page,
-			@RequestParam(defaultValue = "5") int size){
-		
+	public ResponseEntity<List<InterviewBasic>> getPaginatedInterviewBasicTitle(
+			@PathVariable(name = "title") String title, @RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "5") int size) {
+
 		Page<Interview> interviewPage = iSer.getPaginatedInterviewBasicTitle(title, page, size);
-		List<InterviewBasic> interviews_basic = interviewPage.stream().map(this::ConvertInterview).collect(Collectors.toList());
-		
-		return new ResponseEntity<>(interviews_basic,HttpStatus.OK);
-		
+		List<InterviewBasic> interviews_basic = interviewPage.stream().map(this::ConvertInterview)
+				.collect(Collectors.toList());
+
+		return new ResponseEntity<>(interviews_basic, HttpStatus.OK);
+
 	}
-	
-	/*ROLE RH
-	  Añadir una nueva entrevista*/
+
+	/*
+	 * ROLE RH Añadir una nueva entrevista
+	 */
 	@PostMapping("/addInterview")
 	public ResponseEntity<Interview> addInterview(@RequestBody Interview i) {
 		Interview i1 = iSer.addInterview(i);
-		return new ResponseEntity<>(i1,HttpStatus.OK);
+		return new ResponseEntity<>(i1, HttpStatus.OK);
 	}
-	
-	/*ROLE RH
-	  Editar una entrevista*/
+
+	/*
+	 * ROLE RH Editar una entrevista
+	 */
 	@PutMapping("/editInterview/{id}")
-	public Interview updateInterview(@PathVariable(name="id") Integer id, @RequestBody Interview i) {
-		
+	public Interview updateInterview(@PathVariable(name = "id") Integer id, @RequestBody Interview i) {
+
 		Interview i1 = iSer.listById(id);
 		Interview i2 = new Interview();
-		
+
 		if (i.getDescription() != null) {
 			i1.setDescription(i.getDescription());
 		}
 		if (i.getTitle() != null) {
-			i1.setTitle(i.getTitle());	
+			i1.setTitle(i.getTitle());
 		}
 		if (i.getTests() != null) {
 			i1.setTests(i.getTests());
@@ -112,40 +116,43 @@ public class InterviewController {
 		if (i.getEnd_date() != null) {
 			i1.setEnd_date(i.getEnd_date());
 		}
-		
+
 		i2 = iSer.updateInterview(i1);
-		
+
 		return i2;
 	}
-	
-	/*ROLE RH
-	  Eliminar una entrevista*/
+
+	/*
+	 * ROLE RH Eliminar una entrevista
+	 */
 	@DeleteMapping("/deleteInterview/{id}")
-	public void deleteByIdInterview (@PathVariable(name="id") Integer id) {
+	public void deleteByIdInterview(@PathVariable(name = "id") Integer id) {
 		iSer.deleteByIdInterview(id);
 	}
-	
-	/*ROLE RH
-	  Entrevista con informacionpara el usuario de rh*/
+
+	/*
+	 * ROLE RH Entrevista con informacionpara el usuario de rh
+	 */
 	@GetMapping("/show_interview_rh/{id}")
-	public Interview show_interview_rh(@PathVariable(name="id") Integer id) {
-		System.out.println("INTERVIEW"+iSer.listById(id).getTests());
+	public Interview show_interview_rh(@PathVariable(name = "id") Integer id) {
+		System.out.println("INTERVIEW" + iSer.listById(id).getTests());
 		return iSer.listById(id);
 	}
-		
-	/*ROLE USUARIO
-	  Entrevista con informacion basica*/
+
+	/*
+	 * ROLE USUARIO Entrevista con informacion basica
+	 */
 	@GetMapping("/show_interview_user/{id}")
-	public ResponseEntity<InterviewUserResponse> show_interview_user(@PathVariable(name="id") Integer id) {
-		org.springframework.security.core.Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+	public ResponseEntity<InterviewUserResponse> show_interview_user(@PathVariable(name = "id") Integer id) {
+		org.springframework.security.core.Authentication authentication = SecurityContextHolder.getContext()
+				.getAuthentication();
 		User current_user = iuuSer.findByEmail(authentication.getName());
 		int user_id = current_user.getId();
 		Interview interview = iSer.listById(id);
-		UserInterview user_interview = iuSer.findByUserIdAndInterviewId(user_id,id); 
-		List<UserTest> test_user = iutSer.findByUserIdAndInterviewId(user_id,id);
-		InterviewUserResponse response = new InterviewUserResponse(interview, user_interview,test_user);
+		UserInterview user_interview = iuSer.findByUserIdAndInterviewId(user_id, id);
+		List<UserTest> test_user = iutSer.findByUserIdAndInterviewId(user_id, id);
+		InterviewUserResponse response = new InterviewUserResponse(interview, user_interview, test_user);
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
-		
 
 }
--- deepcodeskill/src/main/java/com/dcs/security/SecurityConfig.java
+++ deepcodeskill/src/main/java/com/dcs/security/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		boolean security = true;
 		if (security) {
-			return http.csrf(csfr -> csfr.disable()).cors(cors ->cors.disable()).authorizeHttpRequests().requestMatchers(UN_SECURED_URLs).permitAll().and()
+			return  http.cors().and().csrf().disable().authorizeHttpRequests().requestMatchers(UN_SECURED_URLs).permitAll().and()
 					.authorizeHttpRequests().requestMatchers(SECURED_ADMIN).hasAuthority("admin")
 					.requestMatchers(SECURED_USER).hasAuthority("user").requestMatchers(SECURED_HR_AND_USER).hasAnyAuthority("hr","user")
 					.requestMatchers(SECURED_HR).hasAuthority("hr").anyRequest().authenticated().and()
